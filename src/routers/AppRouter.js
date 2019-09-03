c4920e3776468434773abebe754c1516fce70474
Lartwel <mlartwel@gmail.com>
2019-09-03 18:03:47 +0200

    updates react, enzyme, and raf and tweaked history importing in appRouter

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
-import createHistory from 'history/createBrowserHistory';
+import { createBrowserHistory } from 'history';
 import NotFoundPage from '../components/NotFoundPage';
 import PrivateRoute from './PrivateRoute';
 import PublicRoute from './PublicRoute';
 import DashboardPage from '../components/DashboardPage';
 import LoginPage from '../components/LoginPage';
 
-export const history = createHistory();
+export const history = createBrowserHistory();
 
 const AppRouter = () => (
   <Router history={history}>
